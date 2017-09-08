02899283ee1d4af9aed45c2580a7ed5a923c9189
Heninapso <heninapso@gmail.com>
2017-09-08 12:08:45 +0200

    chat working

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907133227) do
+ActiveRecord::Schema.define(version: 20170907142057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chat_rooms", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "chat_room_id"
+    t.string   "content"
+    t.string   "avatar_url"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["chat_room_id"], name: "index_messages_on_chat_room_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -28,8 +45,12 @@ ActiveRecord::Schema.define(version: 20170907133227) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "messages", "chat_rooms"
+  add_foreign_key "messages", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database'
+ChatRoom.destroy_all
+User.destroy_all
+Message.destroy_all
+
+
+puts'Creating Chatroom'
+chat_room = ChatRoom.create!(name: "general")
+
+puts 'Creating Users'
+donnie = User.create!(
+  name: "Donnie",
+  avatar_url: "https://dianaashworth.files.wordpress.com/2014/03/041squaresheep.jpg",
+  email: "donnie@donnie.com",
+  password: "azerty"
+)
+
+yellow = User.create!(
+  name: "Yellow",
+  avatar_url: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Bear_Square.JPG",
+  email: "yellow@yellow.com",
+  password: "azerty"
+)
+
+puts 'Creating messages'
+message1 = Message.create!(
+  content: "Hello",
+  chat_room: chat_room,
+  user: donnie
+)
+
+message2 = Message.create!(
+  content: "You stink",
+  chat_room: chat_room,
+  user: yellow
+)
+
+puts 'seeds completed'
